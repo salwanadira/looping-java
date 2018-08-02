bec63766cad14a8e5f1705e8715658e7664f9a07
SALWA NADIRA <SALWA NADIRA@LAPTOP-6GBIJ8E9>
2018-08-02 07:51:55 +0700

    For While Do-While

---
+++ src/j08/Contoh1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j08;
+
+/**
+ *
+ * @author SALWA NADIRA
+ */
+public class Contoh1 {
+    public static void main(String[] args)
+    {
+        //PERULANGAN FOR
+        int x;
+        for(x = 1; x<= 5; x++)
+        {
+            System.out.println("SMK Telkom Malang");
+        }
+        
+        //PERULANGAN WHILE
+        int i = 1;
+        while(1 <= 5)
+        {
+            System.out.println("SMK Telkom Malang");
+            i++;
+        }
+        
+        //PERULANGAN DO WHILE
+        int j = 1;
+        do
+        { 
+            System.out.println("SMK Telkom Malang");
+            j++;
+        }
+        while(j <= 5);
+    }
+}
